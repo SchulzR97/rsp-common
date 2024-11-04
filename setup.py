db98cb73a6d0292fb9a123312d904e98c9a018e4
Robert Schulz <schulzr@gmail.com>
2024-11-04 11:04:33 +0100

    0.0.26 - added some colors

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.25' 
+VERSION = '0.0.26' 
 DESCRIPTION = 'Some basic functionalities'
 with open('README.md') as f:
     LONG_DESCRIPTION = f.read()
