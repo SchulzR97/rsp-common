d85369b5a66c20803fb6f489b7d8b8ce478ec066
Robert Schulz <schulzr@gmail.com>
2025-01-23 16:52:34 +0100

    0.0.31 - bug fix in console.print_c -> disable line feed did not work

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.30' 
+VERSION = '0.0.31' 
 DESCRIPTION = 'Some basic functionalities'
 with open('README.md') as f:
     LONG_DESCRIPTION = f.read()
